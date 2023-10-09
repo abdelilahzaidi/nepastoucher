c42b0087a481d93e4d21e15ed9f1404d03c73c50
abdelilahzaidi <abdelilahzaidi1983@gmail.com>
2023-10-09 09:38:15 +0200

    add template seance-new

--- src/app/admin/seance-folder/seance-new/seance-new.component.ts
+++ src/app/admin/seance-folder/seance-new/seance-new.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { SeanceService } from 'src/app/services/seance/seance.service';
 
 @Component({
   selector: 'app-seance-new',
   templateUrl: './seance-new.component.html',
   styleUrls: ['./seance-new.component.css']
 })
-export class SeanceNewComponent {
+export class SeanceNewComponent implements OnInit {
 
+  errorMessage: any;
+  response$ :any;
+  constructor(private fb : FormBuilder, private seanceService : SeanceService,private router: Router, private $http: HttpClient){}
+  seanceForm : FormGroup = this.fb.group({
+    idDateCour: new FormControl('', [Validators.required]),
+    idHoraire:new FormControl('', [Validators.required]),
+    idCour:new FormControl('', [Validators.required]),
+  })
+
+  //grades$: Observable<any[]> = of([])
+  ngOnInit(){
+    //this.grades$ = this.$http.get<any[]>('http://localhost:3001/level')
+  }
+  async submit() {
+    console.log('user / submit', this.seanceForm.value);
+
+    this.seanceService.createSeance(this.seanceForm.value)
+      .subscribe(
+        (res) => {
+          console.log('Réponse du serveur :', res);
+          this.response$ = res; // Si vous avez besoin de stocker la réponse
+          alert("Programme ajouté");
+          this.router.navigate(['/admin/program-list']);
+        },
+        (error) => {
+          this.errorMessage=error;
+          console.error('Une erreur s\'est produite lors de la requête :', error);
+          // Traitez l'erreur comme vous le souhaitez ici
+        }
+      );
+  }
 }
